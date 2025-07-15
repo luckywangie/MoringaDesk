1d5ddfffd7447ae89df114d0cdad55ceb81467af
Grace Nduta <ndutak5674@gmail.com>
2025-07-15 13:06:57 +0500

    added answers class and questions class

--- BACK-END/models.py
+++ BACK-END/models.py
@@ -16,9 +16,43 @@ class User(db.Model):
     is_admin = db.Column(db.Boolean, default=False)
     
     # Relationships
+    
 
     def __repr__(self):
         return f'<User {self.username}>'
     
-
+class Question (db.Model):
+    __table__name__ = 'questions'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    is_solved = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    language = db.Column(db.String(50), nullable=False)
+    
+    # Relationships
+    user = db.relationship('User', backref='questions')
+    question_tags = db.relationship('QuestionTag', backref='question', cascade='all, delete-orphan')
+    category = db.relationship('Category', backref='questions')
+    def __repr__(self):
+        return f'<Question {self.title}>'
+    
+class Answers (db.Model):
+    __tablename__ = 'answers'
+    id = db.Column(db.Integer, primary_key=True)
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    is_approved = db.Column(db.Boolean, default=False)
+    
+    # Relationships
+    user = db.relationship('User', backref='answers')
+    question = db.relationship('Question', backref='answers')
+    
+    def __repr__(self):
+        return f'<Answer {self.id} for Question {self.question_id}>'
+    
 
