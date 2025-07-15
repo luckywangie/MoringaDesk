c17e6d05cae8d2b3d95101e0370a02602d87b458
lucky wangie <wangiemamati@gmail.com>
2025-07-15 10:41:42 +0300

    class related questions added

--- BACK-END/models.py
+++ BACK-END/models.py
@@ -14,4 +14,8 @@ class Votes(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     solution_id = db.Column(db.Integer, db.ForeignKey('answer.id'))
 
+class RelatedQuestions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
+    related_question_id = db.Column(db.Integer)
 
